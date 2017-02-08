de0b0c69625864a47f8778045aea17cc896f2875
jacobvenable <jacob.venable@gmail.com>
2017-02-08 16:13:43 -0500

    Initial commit. Since project was initially created and managed without Git, the commit is going to contain all project files.

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+// Include gulp and gulp 
+var gulp = require('gulp'),
+    //general
+    ifelse = require('gulp-if-else'),
+    connect = require('gulp-connect'),
+    //html
+    fileinclude = require('gulp-file-include'),
+    htmlPrettify = require('gulp-html-prettify'),
+    //css
+	sass = require('gulp-sass');
+
+//initiate variables
+var env,
+	htmlSources,
+	scssSources,
+	outputDir;
+
+//retrieve environment variable. 'development' by default
+env = process.env.NODE_ENV || 'development';
+
+//set the output directory based on enviroment variable
+if(env==='development')
+{ //node environment variable  set to development
+	outputDir = 'builds/development/'; //set ouput directory to development
+}
+else
+{ //node environment variable is not set to development
+	outputDir = 'builds/production/'; //set output directory to production
+}
+
+htmlSources = ['components/html/templates/**/*.html'];
+templateSources = ['components/html/modules/**/*.html'];
+scssSources = ['components/css/**/*.scss'];
+
+//HTML task
+gulp.task('html', function(){
+    return gulp.src(htmlSources)
+        .pipe(fileinclude({
+          prefix: '@@',
+          basepath: '@file'
+        }))
+        .pipe(htmlPrettify({indent_char:' ',indent_size:4}))
+        .pipe(gulp.dest(outputDir+'templates/'))
+        .pipe(connect.reload())
+});
+
+//PostCSS task
+gulp.task('sass', function(){
+    return gulp.src(scssSources)
+        .pipe(ifelse(env === 'production',
+            function(){
+                return sass({outputStyle:'compressed'}).on('error',sass.logError)
+            },
+            function(){
+                return sass({outputStyle:'expanded'}).on('error',sass.logError)
+            }
+        ))
+        .pipe(gulp.dest(outputDir+'css/'))
+        .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+    gulp.watch(htmlSources,['html']);
+    gulp.watch(templateSources,['html']);
+    gulp.watch(scssSources,['sass']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: outputDir,
+        livereload: true
+    });
+});
+
+// Default Task
+/*gulp.task('default', ['fileinclude','html','lint','css','scripts','watch']);*/
+
+gulp.task('default',['html','sass','connect','watch']);
