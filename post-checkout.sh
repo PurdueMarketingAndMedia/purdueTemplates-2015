067de5f266af128ae54a800dbc302fb108cdd588
Blake Holaday <bholaday@purdue.edu>
2018-05-31 11:13:16 -0400

    Add message informing user that the file is dynamically generated.

--- post-checkout.sh
+++ post-checkout.sh
@@ -5,6 +5,7 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 
 # dynamically build .gitignore
 printf "" > .gitignore
+echo "# This file is dynamically generated. To edit the list of ignored items, edit post-checkout.sh, save, and rerun npm run setup." >> .gitignore
 echo ".DS_Store" >> .gitignore
 echo "node_modules" >> .gitignore
 echo ".tmp" >> .gitignore
