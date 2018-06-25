64a8f512685056aa6482251a0c61f3bc127dd580
jacobvenable <jacob.venable@gmail.com>
2018-06-25 13:21:50 -0600

    Update the gulpfile to handle zipping the production folder.
    Move all HTML and SASS compiling task code into separate functions to be used outside their gulp task.
    
    Have each function return a promise that is used to synchronously compile the production builds during the zip task.
    
    Create the zipBuild to handle initiating the compiling of HTML and SASS and handling their returned promises to only zip when complete.

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,10 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
 
     //async
-    async = require('async');
+    async = require('async'),
+
+    //zip
+    gulpZip = require('gulp-zip');
 
 // set variables
 var env,
@@ -45,74 +48,136 @@ scssSources = ['components/css/**/*.scss'];
 
 // html task
 gulp.task('html', function(){
-    async.series([
-        function(next)
-        {
-            gulp.src(htmlSources)
-                // include module files
-                .pipe(fileinclude({
-                  prefix: '@@',
-                  basepath: '@file'
-                }))
-                // clean up html
-                .pipe(htmlPrettify({
-                    indent_char:' ',
-                    indent_size:4
-                }))
-                // save into builds
-                .pipe(gulp.dest(outputDir+'templates/'))
-                .on('end',next);
-        },
-        function(next)
-        {
-            // reload on any change in components
-            gulp.src(componentSources)
-                .pipe(connect.reload());
-        }
-    ]);
+    return htmlBuild();
 });
+
+function htmlBuild(zip = false){
+    //return a promise that resolves when the HTML finishes compiling
+    return new Promise((resolve, reject) => {
+        async.series([
+            function(next)
+            {
+                gulp.src(htmlSources)
+                    // include module files
+                    .pipe(fileinclude({
+                      prefix: '@@',
+                      basepath: '@file'
+                    }))
+                    // clean up html
+                    .pipe(htmlPrettify({
+                        indent_char:' ',
+                        indent_size:4
+                    }))
+                    // save into respective builds folder
+                    .pipe(gulp.dest(outputDir+'templates/'))
+                    .on('end',next);
+            },
+            function(next)
+            {
+                // reload the page if this was not called by the "zip" task
+                !zip ? gulp.src(componentSources).pipe(connect.reload()): null;
+                //resolve the promise
+                resolve(true);
+            }
+        ]);
+    });
+}
 
 // sass task
 gulp.task('sass', function(){
     if (env === 'production') {
-        sassBuild(true);
+        return sassBuild(true);
     } else { // assume it's the dev environment
-        sassBuild(false);
+        return sassBuild(false);
     }
 });
 
-function sassBuild(production = false){
-    async.series([
-        function(next) {
-            if(production)
-            {
-                gulp.src(scssSources)
-                    .pipe(sass({outputStyle:'compressed'}).on('error',sass.logError))
-                    .pipe(postcss([ autoprefixer, pseudoelements ]))
-                    .pipe(gulp.dest(outputDir+'css/'))
-                    .on('end',next);
+function sassBuild(production = false,zip = false){
+    return new Promise((resolve, reject) => {
+        async.series([
+            function(next) {
+                if(production)
+                { //compile the production CSS
+                    gulp.src(scssSources)
+                        .pipe(sass({outputStyle:'compressed'}).on('error',sass.logError))
+                        .pipe(postcss([ autoprefixer, pseudoelements ]))
+                        .pipe(gulp.dest(outputDir+'css/'))
+                        .on('end',next);
+                }
+                else
+                { //compile the development CSS
+                    gulp.src(scssSources)
+                        .pipe(sourcemaps.init())
+                        .pipe(sass({outputStyle:'expanded'}).on('error',sass.logError))
+                        .pipe(sourcemaps.write('./sourcemaps'))
+                        .pipe(gulp.dest(outputDir+'css/'))
+                        .on('end',next);
+                }
+            },
+            function(next) {
+                // reload the page if this was not called by the "zip" task
+                !zip ? gulp.src(componentSources).pipe(connect.reload()) : null;
+                //resolve the promise
+                resolve(true);
             }
-            else{
-                gulp.src(scssSources)
-                    .pipe(sourcemaps.init())
-                    .pipe(sass({outputStyle:'expanded'}).on('error',sass.logError))
-                    .pipe(sourcemaps.write('./sourcemaps'))
-                    .pipe(gulp.dest(outputDir+'css/'))
-                    .on('end',next);
-            }
-        },
-        function(next) {
-            // reload on any change in components
-            gulp.src(componentSources)
-                .pipe(connect.reload());
-        }
-    ]);
-    return true;
+        ]);
+    });
 }
 
 gulp.task('zip',function(){
+    return new Promise((resolve, reject) => {
+        let compileHtml = htmlBuild(true);
+        compileHtml.then(
+            () => {
+                //successfully compiled HTML
+                let compileSass = sassBuild(true,true);
+                compileSass.then(
+                    () => {
+                        //successfully compiled SASS
+                        let zipFiles = zipBuild();
+                        zipFiles.then(
+                            () => {
+                                //successfully zipped production files
+                                resolve(true);
+                            }
+                        ).catch(
+                            () => {
+                                //failed to zip production files
 
+                            }
+                        );
+                    }
+                ).catch(
+                    ( )=>{
+                        //failed to compile SASS
+                        console.log('sass failed to compile');
+                    }
+                );
+            }
+        ).catch(
+            ()=>{
+                //failed to compile HTML
+            }
+        );
+    });
 });
+
+function zipBuild(){
+    return new Promise((resolve,reject) => {
+        async.series([
+            function(next) {
+                gulp.src('builds/production/*')
+                    .pipe(gulpZip('vX.X.X.zip'))
+                    .pipe(gulp.dest('builds'))
+                    .on('end',next);
+            },
+            function(next) {
+                //resolve the promise
+                resolve(true);
+            }
+        ]);
+    });
+}
 
 // watch for changes
 gulp.task('watch', function() {
