8bc8712ddac6725ca08346515fdce960de3bbf7c
Jesse Young <young295@purdue.edu>
2019-05-06 10:55:23 -0400

    Add build scripts and handlebars structure

--- html-render.js
+++ html-render.js
@@ -1,5 +1,6 @@
 const handlebars = require('handlebars');
 const fs = require('fs');
+const path = require('path')
 const pretty = require('pretty');
 
 // const template = fs.readFileSync('src/html/pageRender.handlebars', 'utf8');
@@ -40,26 +41,56 @@ const pretty = require('pretty');
 //   });
 // });
 
-const recursiveRead = (dir) => {
-  let results = []
+const recursiveRead = (dir, ext) => {
+  let results = {}
 
   const items = fs.readdirSync(dir)
 
   for (const item of items) {
 
-    if (item.indexOf('.handlebars') === -1) {
+    if (item.indexOf(ext) === -1) {
       let newDirPath = `${dir}/${item}`
 
-      const recursResults = readComponents(newDirPath)
-      results = [...results, ...recursResults]
+      const recursResults = recursiveRead(newDirPath, ext)
+      results = {...results, ...recursResults}
     } else {
-      results.push(item)
+      results[`${dir}/${item}`] = item
     }
   }
 
   return results
 }
 
-console.log(recursiveRead('src/html/components'))
+// console.log(recursiveRead('src/html/components'))
 
+const registerAllPartials = (filesObj) => {
+  for (file in filesObj) {
+    const partialName = filesObj[file].replace(/\.[^./]+$/, '');
+    const partialContent = fs.readFileSync(file, 'utf8');
+    handlebars.registerPartial(partialName, partialContent);
+  }
+}
+
+const compileViews = (filesObj) => {
+  const template = fs.readFileSync('src/html/pageRender.handlebars', 'utf8');
+  const compiledTemplate = handlebars.compile(template);
+  const isDev = process.env.NODE_ENV === 'development'
+  for (file in filesObj) {
+    console.log(file)
+    const outName = isDev ? `new_builds${file.replace('src', '').replace('.json', '.html').replace('/views', '')}` : `new_builds/production${file.replace('src', '').replace('.json', '.html').replace('/views', '')}`
+    const viewJSON = JSON.parse(fs.readFileSync(file, 'utf8'));
+    const compiled = compiledTemplate(viewJSON);
+    const finalOut = pretty(compiled, { ocd: true });
+    fs.mkdirSync(path.dirname(outName), {recursive: true})
+    fs.writeFileSync(outName, finalOut, {flag: 'w'})
+  }
+}
+
+const filesObjComponents = recursiveRead('src/html/components', '.handlebars')
+const filesObjGlobals = recursiveRead('src/html/globals', '.handlebars')
+const filesObjViews = recursiveRead('src/html/views', '.json')
+registerAllPartials(filesObjComponents)
+registerAllPartials(filesObjGlobals)
+
+compileViews(filesObjViews)
 
