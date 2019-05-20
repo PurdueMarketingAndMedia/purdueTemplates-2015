6c17bfd02fa805d3aecc230902249a6cb1e2bbc0
Jesse Young <young295@purdue.edu>
2019-05-20 11:56:13 -0400

    complete gold bar mobile toggle

--- src/js/mm/templates/toggle.js
+++ src/js/mm/templates/toggle.js
@@ -1,131 +1,131 @@
 
-//toggle gold bar
-const toggleMenu = (e, identifier, goldBarMobile) => {
-    e = e.target
-    const expanded = e.getAttribute('aria-expanded') === "false" ? true : false
-    e.setAttribute('aria-expanded', expanded)
+// //toggle gold bar
+// const toggleMenu = (e, identifier, goldBarMobile) => {
+//     e = e.target
+//     const expanded = e.getAttribute('aria-expanded') === "false" ? true : false
+//     e.setAttribute('aria-expanded', expanded)
 
-    const el = document.querySelector(identifier)
-    const currAttr = (window.getComputedStyle ? getComputedStyle(el, null) : el.currentStyle).display
-    if (currAttr && currAttr === 'flex') {
-        el.removeAttribute('style')
-    } else {
-        el.style.display = 'flex'
-    }
+//     const el = document.querySelector(identifier)
+//     const currAttr = (window.getComputedStyle ? getComputedStyle(el, null) : el.currentStyle).display
+//     if (currAttr && currAttr === 'flex') {
+//         el.removeAttribute('style')
+//     } else {
+//         el.style.display = 'flex'
+//     }
 
-    if (goldBarMobile) {
-        let height = (window.getComputedStyle ? getComputedStyle(el, null) : el.currentStyle).maxHeight
-        if (height === '48px') {
-            el.style.maxHeight = '900px'
-        } else if (height == '900px') {
-            el.style.maxHeight = height === '48px'
-        }
-        setTimeout(() => {
-            height = (window.getComputedStyle ? getComputedStyle(el, null) : el.currentStyle).maxHeight
-        }, 1000)
-    }
-}
-document.querySelector(".header__goldBar--inner>button").addEventListener('click', (e) => { toggleMenu(e, `.header__goldBar--inner`, true) })
-document.querySelector(".header__goldBar__findInfoFor>button").addEventListener('click', (e) => { toggleMenu(e, `#findInfoFor`, true) })
-//toggle function
+//     if (goldBarMobile) {
+//         let height = (window.getComputedStyle ? getComputedStyle(el, null) : el.currentStyle).maxHeight
+//         if (height === '48px') {
+//             el.style.maxHeight = '900px'
+//         } else if (height == '900px') {
+//             el.style.maxHeight = height === '48px'
+//         }
+//         setTimeout(() => {
+//             height = (window.getComputedStyle ? getComputedStyle(el, null) : el.currentStyle).maxHeight
+//         }, 1000)
+//     }
+// }
+// document.querySelector(".header__goldBar--inner>button").addEventListener('click', (e) => { toggleMenu(e, `.header__goldBar--inner`, true) })
+// document.querySelector(".header__goldBar__findInfoFor>button").addEventListener('click', (e) => { toggleMenu(e, `#findInfoFor`, true) })
+// //toggle function
 
-const toggle = (e) => {
-    let clicked = e.currentTarget
-    const className = (term) => {
-        return clicked.classList.contains(term);
-    }
-    switch (true) {
-        case className('dropdown-button'):
-            const dropTarget = clicked.parentNode.children[1]
-            if (dropTarget.classList[0] !== 'header__mainNav--dropdown2') {
-                const outerDropdowns = document.querySelectorAll('.header__mainNav--dropdown1')
-                const innerDropdowns = document.querySelectorAll('.header__mainNav--dropdown2')
-                const dropdowns = [...outerDropdowns, ...innerDropdowns]
-                for (const dropdown of dropdowns) {
-                    if (dropdown !== dropTarget) {
-                        dropdown.removeAttribute('style')
-                    }
-                }
-            } else if (dropTarget.classList[0] === 'header__mainNav--dropdown2') {
-                const innerDropdowns = document.querySelectorAll('.header__mainNav--dropdown2')
-                for (const dropdown of innerDropdowns) {
-                    if (dropdown !== dropTarget) {
-                        dropdown.removeAttribute('style')
-                    }
-                }
-            }
-            let display = (window.getComputedStyle ? getComputedStyle(dropTarget, null) : dropTarget.currentStyle).display
-            if (display === "flex") {
-                dropTarget.removeAttribute('style')
-            } else {
-                dropTarget.style.display = 'flex'
-            }
-            break
+// const toggle = (e) => {
+//     let clicked = e.currentTarget
+//     const className = (term) => {
+//         return clicked.classList.contains(term);
+//     }
+//     switch (true) {
+//         case className('dropdown-button'):
+//             const dropTarget = clicked.parentNode.children[1]
+//             if (dropTarget.classList[0] !== 'header__mainNav--dropdown2') {
+//                 const outerDropdowns = document.querySelectorAll('.header__mainNav--dropdown1')
+//                 const innerDropdowns = document.querySelectorAll('.header__mainNav--dropdown2')
+//                 const dropdowns = [...outerDropdowns, ...innerDropdowns]
+//                 for (const dropdown of dropdowns) {
+//                     if (dropdown !== dropTarget) {
+//                         dropdown.removeAttribute('style')
+//                     }
+//                 }
+//             } else if (dropTarget.classList[0] === 'header__mainNav--dropdown2') {
+//                 const innerDropdowns = document.querySelectorAll('.header__mainNav--dropdown2')
+//                 for (const dropdown of innerDropdowns) {
+//                     if (dropdown !== dropTarget) {
+//                         dropdown.removeAttribute('style')
+//                     }
+//                 }
+//             }
+//             let display = (window.getComputedStyle ? getComputedStyle(dropTarget, null) : dropTarget.currentStyle).display
+//             if (display === "flex") {
+//                 dropTarget.removeAttribute('style')
+//             } else {
+//                 dropTarget.style.display = 'flex'
+//             }
+//             break
 
-        default:
-            switch (clicked.id) {
-                case "mainNavMo":
-                    console.log('clicked')
-                    const dropTarget = clicked.parentNode.children[1]
-                    let height = (window.getComputedStyle ? getComputedStyle(dropTarget, null) : dropTarget.currentStyle).maxHeight
-                    if (height === '0px') {
-                        dropTarget.style.maxHeight = '1900px'
-                    } else if (height !== '0px') {
-                        dropTarget.style.maxHeight = '0px'
-                    }
-                    break
-                default:
-                    break
-            }
-            break
-    }
-    const dropTarget = e.target.parentNode.children[1]
-}
+//         default:
+//             switch (clicked.id) {
+//                 case "mainNavMo":
+//                     console.log('clicked')
+//                     const dropTarget = clicked.parentNode.children[1]
+//                     let height = (window.getComputedStyle ? getComputedStyle(dropTarget, null) : dropTarget.currentStyle).maxHeight
+//                     if (height === '0px') {
+//                         dropTarget.style.maxHeight = '1900px'
+//                     } else if (height !== '0px') {
+//                         dropTarget.style.maxHeight = '0px'
+//                     }
+//                     break
+//                 default:
+//                     break
+//             }
+//             break
+//     }
+//     const dropTarget = e.target.parentNode.children[1]
+// }
 
-//Toggle drop-downs on the top nav bar
-const navDropdowns = document.querySelectorAll(`.dropdown-button`)
+// //Toggle drop-downs on the top nav bar
+// const navDropdowns = document.querySelectorAll(`.dropdown-button`)
 
-for (const dropdown of navDropdowns) {
-    dropdown.addEventListener('click', (e) => { toggle(e) })
-}
+// for (const dropdown of navDropdowns) {
+//     dropdown.addEventListener('click', (e) => { toggle(e) })
+// }
 
-document.addEventListener('click', (e) => {
-    if (e.target.tagName !== "A" && e.target.tagName !== "BUTTON") {
-        const outerDropdowns = document.querySelectorAll('.header__mainNav--dropdown1')
-        const innerDropdowns = document.querySelectorAll('.header__mainNav--dropdown2')
-        const dropdowns = [...outerDropdowns, ...innerDropdowns]
-        for (const dropTarget of dropdowns) {
-            dropTarget.removeAttribute('style')
-        }
-    }
-})
+// document.addEventListener('click', (e) => {
+//     if (e.target.tagName !== "A" && e.target.tagName !== "BUTTON") {
+//         const outerDropdowns = document.querySelectorAll('.header__mainNav--dropdown1')
+//         const innerDropdowns = document.querySelectorAll('.header__mainNav--dropdown2')
+//         const dropdowns = [...outerDropdowns, ...innerDropdowns]
+//         for (const dropTarget of dropdowns) {
+//             dropTarget.removeAttribute('style')
+//         }
+//     }
+// })
 
-document.querySelector('#mainNavMo').addEventListener('click', (e) => { toggle(e) })
+// document.querySelector('#mainNavMo').addEventListener('click', (e) => { toggle(e) })
 
-const reset = (oldWidth) => {
-    const footerHeadings = document.querySelectorAll(".footer__info-links--column>h3>button.accordion__heading--footer");
-    const width = document.body.clientWidth;
-    const goldBarDisplay = document.querySelector(".header__goldBar--menus")
-    const goldBarHeight = document.querySelector(".header__goldBar--inner")
-    const findInfoFor = document.querySelector("#findInfoFor")
+// const reset = (oldWidth) => {
+//     const footerHeadings = document.querySelectorAll(".footer__info-links--column>h3>button.accordion__heading--footer");
+//     const width = document.body.clientWidth;
+//     const goldBarDisplay = document.querySelector(".header__goldBar--menus")
+//     const goldBarHeight = document.querySelector(".header__goldBar--inner")
+//     const findInfoFor = document.querySelector("#findInfoFor")
 
-    if (width > 768) {
-        goldBarDisplay.removeAttribute('style')
-        goldBarHeight.removeAttribute('style')
-        document.querySelector('.header__mainNav--main').removeAttribute('style')
-    } else {
-        findInfoFor.removeAttribute('style')
-    }
-}
+//     if (width > 768) {
+//         goldBarDisplay.removeAttribute('style')
+//         goldBarHeight.removeAttribute('style')
+//         document.querySelector('.header__mainNav--main').removeAttribute('style')
+//     } else {
+//         findInfoFor.removeAttribute('style')
+//     }
+// }
 
-window.addEventListener('resize', () => {
-    reset(width);
-    let timer;
-    clearInterval(timer);
-    timer = setTimeout(() => {
-        width = document.body.clientWidth
-    }, 250);
-});
+// window.addEventListener('resize', () => {
+//     reset(width);
+//     let timer;
+//     clearInterval(timer);
+//     timer = setTimeout(() => {
+//         width = document.body.clientWidth
+//     }, 250);
+// });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -134,19 +134,19 @@ window.addEventListener('resize', () => {
 //toggle function
 const toggleNew = (e) => {
     let clicked = e.currentTarget
+    console.log(clicked)
     const checkClassName = (el, term) => {
         return el.classList.contains(term);
     }
     switch (true) {
-        case checkClassName(clicked, 'accordion__heading'):
+        case checkClassName(clicked, 'accordion__heading'): // footer accordion
             const expanded = clicked.getAttribute('aria-expanded') === "false" ? true : false;
             clicked.setAttribute('aria-expanded', expanded);
             const contentId = clicked.getAttribute('aria-controls');
 
             
             let icons = clicked.querySelectorAll('svg');
-            let content = document.querySelector('#' + contentId);
-            console.log(content.getAttribute('state-animating'))
+            const content = document.querySelector('#' + contentId);
             const currAttr = window.getComputedStyle(content).getPropertyValue('display');
             if (currAttr && currAttr === 'flex' && content.getAttribute('state-animating') === null) {
                 icons.forEach((icon) => {
@@ -169,6 +169,31 @@ const toggleNew = (e) => {
                 }, 200)
                 content.style.height = content.scrollHeight+"px";
             }
+            break
+        case  checkClassName(clicked, 'header__goldBar--moButton'): // gold bar mobile menu
+            const goldBarContent = document.querySelector('.header__goldBar--menus')
+            const goldBarContainer = document.querySelector('.header__goldBar--inner')
+            const currGoldBarAttr = window.getComputedStyle(goldBarContent).getPropertyValue('display');
+
+            if (currGoldBarAttr && currGoldBarAttr === 'flex' && goldBarContent.getAttribute('state-animating') === null) {
+                goldBarContainer.style.height = 0;
+                goldBarContent.setAttribute('state-animating', 'true')
+                setTimeout(() => {
+                    hide(goldBarContent)
+                    goldBarContent.removeAttribute('state-animating')
+                    goldBarContent.style.height = 0;
+                }, 200)
+            } else if(goldBarContent.getAttribute('state-animating') === null) {
+                const goldBarHeight = parseInt(goldBarContainer.scrollHeight)
+                show(goldBarContent);
+                goldBarContent.style.height = `${goldBarContent.scrollHeight}px`
+                goldBarContainer.style.height = `${(parseInt(goldBarContent.style.height) + goldBarHeight + 6).toString()}px`
+                goldBarContent.setAttribute('state-animating', 'true')
+                setTimeout(() => {
+                    goldBarContent.removeAttribute('state-animating')
+                }, 200)
+            }
+
             break
 
     }
@@ -258,3 +283,8 @@ window.addEventListener('resize', () => {
     });
 
 });
+
+
+document.querySelector('.header__goldBar--moButton').addEventListener('click', (e) => {
+    toggleNew(e)
+})
