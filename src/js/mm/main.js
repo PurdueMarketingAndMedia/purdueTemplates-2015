d59a653c7daf7c8615aa45085e8ba27634fe26f8
Yanhua <yanhuazong@gmail.com>
2019-07-24 10:19:54 -0400

    Fix  display of header signature & footer in ie; make toggle function work in ie

--- src/js/mm/main.js
+++ src/js/mm/main.js
@@ -2,6 +2,10 @@ import "core-js/stable"
 import "regenerator-runtime/runtime"
 
 import "./templates/blank.js";
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+                                Element.prototype.webkitMatchesSelector;
+  }
 
 
 
