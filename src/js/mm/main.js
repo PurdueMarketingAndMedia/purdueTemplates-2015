fe6e5cfa004fd67adb048ee4cccbf32a26920063
Jesse Young <young295@purdue.edu>
2019-09-18 10:51:26 -0400

    update toggle.js path

--- src/js/mm/main.js
+++ src/js/mm/main.js
@@ -1,6 +1,6 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
-import "../global/toggle.js";
+import "./global/toggle.js";
 if (!Element.prototype.matches) {
     Element.prototype.matches = Element.prototype.msMatchesSelector ||
                                 Element.prototype.webkitMatchesSelector;
