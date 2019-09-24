03aaade16977660a56c7af0058841f0b429ec76e
Jesse Young <young295@purdue.edu>
2019-09-24 10:30:18 -0400

    move view to correct file location update main js for new build pattern from dev

--- src/js/mm/main.js
+++ src/js/mm/main.js
@@ -1,6 +1,7 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
 import "./global/toggle.js";
+import "./global/tab.js";
 if (!Element.prototype.matches) {
     Element.prototype.matches = Element.prototype.msMatchesSelector ||
                                 Element.prototype.webkitMatchesSelector;
