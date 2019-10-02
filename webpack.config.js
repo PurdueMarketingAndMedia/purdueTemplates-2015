9565a9964b8faaadeb21519fca0270969ded0a64
Jesse Young <young295@purdue.edu>
2019-10-02 11:58:03 -0400

    add prod js build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,57 @@
 var path = require('path')
 
-module.exports = {
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = env => {
+  const devConfig = {
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
-      }
-    ]
-  },
-  entry: {
-    'mm/templates/main': './src/js/mm/main.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'builds/js'),
-    filename: '[name].js'
+      ]
+    },
+    entry: {
+      'mm/templates/main': './src/js/mm/main.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'builds/js'),
+      filename: '[name].js'
+    }
   }
+
+  const prodConfig = {
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    entry: {
+      'mm/templates/main': './src/js/mm/main.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'builds/production/js'),
+      filename: '[name].js'
+    }
+  }
+
+  console.log(env.prod ? 'Running production js build 🚀' : 'Running development js build 🛠')
+
+  return env.prod ? prodConfig : devConfig
 }
